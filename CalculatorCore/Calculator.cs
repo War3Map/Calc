d644f17fa436f40b24d3350dfe2ac15895086f64
War3Map <barcelonasuper@mail.ru>
2019-11-12 14:09:20 +1000

    Правки структуры.

---
+++ CalculatorCore/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCore
+{
+    public class Calculator
+    {
+
+        public decimal PI { get; } = Math.PI;
+
+        public decimal Get_log(decimal number) => Math.Log(number);
+
+        public decimal Get_sin(decimal angle) => Math.Sin(angle);
+
+        public decimal Get_cos(decimal angle) => Math.Cos(angle);
+
+        public decimal Get_tg(decimal angle) => Math.Tan(angle);
+
+        public decimal Get_ctg(decimal angle) => 1 / Math.Tan(angle);
+
+        public decimal Get_factorial(decimal number)
+        {
+            try
+            {
+                if (number == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return number * Get_factorial(number - 1);
+                }
+            }
+            catch (Exception StackOverFlow)
+            {
+                Console.WriteLine("Факториала отрицательного числа не существует!");
+                Console.WriteLine($"Отчет: {StackOverFlow.Message} \n {StackOverFlow.Source}");
+            }
+        }
+        public decimal Get_sqrt(decimal number) => Math.Sqrt(number);
+
+        public decimal ConversionGR(decimal degree) => degree * PI / 180;
+
+        public decimal ConversionRG(decimal radian) => radian * 180 / PI;
+
+        public decimal Get_persent(decimal number, decimal persent) => number / 100 * persent;
+
+        public decimal Add(decimal x, decimal y) => x + y;
+
+        public decimal Sub(decimal x, decimal y) => x - y;
+
+        public decimal Mult(decimal x, decimal y) => x * y;
+
+        public decimal Div(decimal x, decimal y) => x / y;
+
+        public decimal Pow(decimal number, decimal power) => Math.Pow(number, power);
+
+        public decimal MoreSQRT(decimal number, decimal power)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CalculatorCore/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorCore
+{
+    class State
+    {
+
+        public State()
+        {
+
+        }
+
+        public Stack<decimal> Memory { get; set; }
+
+        public void AddToMem(decimal number)
+        {
+            Memory.Push(number);
+        }
+        public decimal ExtractFromMem() => Memory.Pop();
+    }
+}
