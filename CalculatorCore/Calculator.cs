44d9905fa69243dae43f1097cc1b40aae2ecd203
AsvenAAA <edwardgreatmen@mail.ru>
2019-12-14 10:52:07 +1000

    Починил проблему с null ref exception в Calculator. Калькулятор почти готов.

--- CalculatorCore/Calculator.cs
+++ CalculatorCore/Calculator.cs
@@ -9,13 +9,15 @@ namespace CalculatorCore
     public class Calculator
     {
         private State MemoryState { get; set; }
+        private Dictionary<string, Func<decimal, decimal, string>> computeBinaryDictionary;
 
         public Calculator()
         {
             MemoryState = new State();
         }
 
-        public decimal PI { get; } = (decimal)Math.PI;
+        //public decimal PI { get; } = (decimal)Math.PI;
+        public string PI() => Math.PI.ToString(); // Тутпроблема, не оторбразится пока, что либо не будет введено в maindisplay
 
         public decimal Factorial(decimal number)
         {
@@ -25,13 +27,12 @@ namespace CalculatorCore
             {
                 return number * Factorial(number - 1);
             }
-
         }
         public string TrueFactorial(decimal number) => Factorial(number).ToString();
 
 
-        public decimal ToRadian(decimal degree) => degree * PI / 180;
-        public decimal ToDegree(decimal radian) => radian * 180 / PI;
+        //public decimal ToRadian(decimal degree) => degree * PI / 180;
+        //public decimal ToDegree(decimal radian) => radian * 180 / PI;
         public decimal ToPercent(decimal number, decimal percent) => number / 100 * percent;
 
         public void AddToMem(decimal number)
@@ -54,48 +55,44 @@ namespace CalculatorCore
             return MemoryState.ExtractFromMem();
         }
 
-        //public decimal Add(decimal x, decimal y) => x + y;
         public string Add(decimal x, decimal y) => (x + y).ToString();
-
-        //public decimal Sub(decimal x, decimal y) => x - y;
+       
         public string Sub(decimal x, decimal y) => (x - y).ToString();
-
-        //public decimal Mult(decimal x, decimal y) => x * y;
+       
         public string Mult(decimal x, decimal y) => (x * y).ToString();
-
-        //public decimal Div(decimal x, decimal y) => x / y;
+       
         public string Div(decimal x, decimal y) => (x / y).ToString();
-
-        //public decimal Pow(decimal number) => number * number;
+       
         public string Pow(decimal number) => (number * number).ToString();
-
-        //public double NPow(double number, double power) => Math.Pow(number, power);
+       
         public string NPow(decimal number, decimal power) => Math.Pow((double)number, (double)power).ToString();
-
-        //public double SquareRoot(double number) => Math.Sqrt(number);
+       
         public string SquareRoot(decimal number) => Math.Sqrt((double)number).ToString();
-        //public double Root(double number, double power) => Math.Pow(number, 1 / power);
+        
         public string Root(decimal number, decimal power) => Math.Pow((double)number, 1 / (double)power).ToString();
-
-        //public double NaturalLog(double number) => Math.Log(number);
+        
         public string NaturalLog(decimal number) => Math.Log((double)number).ToString();
 
-        //public double DecimalLog(double number) => Math.Log10(number);
         public string DecimalLog(decimal number) => Math.Log10((double)number).ToString();
 
-        //public double Sin(double angle) => Math.Sin(angle);
         public string Sin(decimal angle) => Math.Sin((double)angle).ToString();
 
-        //public double Cos(double angle) => Math.Cos(angle);
         public string Cos(decimal angle) => Math.Cos((double)angle).ToString();
 
-        //public double Tan(double angle) => Math.Tan(angle);
         public string Tan(decimal angle) => Math.Tan((double)angle).ToString();
 
-        //public double Ctg(double angle) => 1 / Math.Tan(angle);
         public string Ctg(decimal angle) => (1 / Math.Tan((double)angle)).ToString();
 
+        private string Negate(decimal number) => (-number).ToString();
 
+        private string Equal(decimal number) => ComputeBinary(number);
+
+        private string ComputeBinary(decimal number)
+        {
+            if (MemoryState.Operation != null)
+                return computeBinaryDictionary[MemoryState.Operation]?.Invoke(MemoryState.CurrentState, number);
+            else return number.ToString();
+        }
 
         public object Compute(object data)
         {
@@ -105,9 +102,8 @@ namespace CalculatorCore
             //TODO: Сократить код
             //Можно сделать словарь с ключом операция
             //Словарь<string operation, Func<decimal,string> operationResult >
-            //decimal.TryParse(tuple.Item2, out number); //Подумать как красиво сделать относительно всех, if на все в крайнем случае
 
-            Dictionary<string, Func<decimal, string>> computeDictiounary = new Dictionary<string, Func<decimal, string>>
+            Dictionary<string, Func<decimal, string>> computeDictionary = new Dictionary<string, Func<decimal, string>>
             {
                 ["SQRT"] = new Func<decimal, string>(SquareRoot),
                 ["TG"] = new Func<decimal, string>(Tan),
@@ -118,9 +114,11 @@ namespace CalculatorCore
                 ["LN"] = new Func<decimal, string>(NaturalLog),
                 ["LOG"] = new Func<decimal, string>(DecimalLog), //Нашел баг, если передаваемое число крайне мало, то может вывести просто "-"
                 ["N!"] = new Func<decimal, string>(TrueFactorial),
-                ["+/-"] = new Func<decimal, string>(Negate)
-                //Нужно доделать PI, нормально сделать Factorial, решить как поступить с бинарными операциями
+                ["+/-"] = new Func<decimal, string>(Negate),
+                ["="] = new Func<decimal, string>(Equal)
+                //Нужно доделать PI
             };
+
             Dictionary<string, Func<decimal, decimal, string>> computeBinaryDictionary = new Dictionary<string, Func<decimal, decimal, string>>
             {
                 ["+"] = new Func<decimal, decimal, string>(Add),
@@ -128,42 +126,36 @@ namespace CalculatorCore
                 ["*"] = new Func<decimal, decimal, string>(Mult),
                 ["/"] = new Func<decimal, decimal, string>(Div),
                 ["x^n"] = new Func<decimal, decimal, string>(NPow),
-                ["NSQRT"] = new Func<decimal, decimal, string>(Root)
+                ["NSQRT"] = new Func<decimal, decimal, string>(Root),
+                ["M+"] = new Func<decimal, decimal, string>(Add),
+                ["M-"] = new Func<decimal, decimal, string>(Sub)
+            };
+            this.computeBinaryDictionary = computeBinaryDictionary;
+
+            Dictionary<string, Func<string>> constantDictionary = new Dictionary<string, Func<string>>
+            {
+                ["PI"] = new Func<string>(PI)
             };
 
             if (decimal.TryParse(tuple.Item2, out number))
             {
-                if (computeDictiounary.ContainsKey(operation))
+                if (computeDictionary.ContainsKey(operation))
                 {
-                    return computeDictiounary[operation].Invoke(number);
+                    return computeDictionary[operation].Invoke(number);
                 }
-                else if (operation != "=")
+                else if (computeBinaryDictionary.ContainsKey(operation))
                 {
-                    MemoryState.CurrentState = number;
-                    MemoryState.SetOperation(tuple.Item1);
-                    return String.Empty;
-
-                    //switch (operation)
-                    //{
-                    //    case "M+":
-
-                    //        if (decimal.TryParse(tuple.Item2, out number))
-                    //        {
-                    //            return Add(MemoryState.PeekFromMem(), number).ToString();
-                    //        }
-                    //        else
-                    //            return 0;
-                    //    case "M-":
-
-                    //        if (decimal.TryParse(tuple.Item2, out number))
-                    //        {
-                    //            return Sub(MemoryState.PeekFromMem(), number).ToString();
-                    //        }
-                    //        else
-                    //            return 0;
-                    //    case "PI":
-                    //        return PI;
-                    #region Закоменченая старая реализация унарных операций на время тестов словаря
+                    if (operation == "M+" || operation == "M-")
+                    {
+                        return computeBinaryDictionary[operation].Invoke(MemoryState.PeekFromMem(), number);
+                    }
+                    else
+                    {
+                        MemoryState.CurrentState = number;
+                        MemoryState.SetOperation(tuple.Item1);
+                        return String.Empty;
+                    }
+                    #region Cтарая реализация унарных операций на время тестов словаря
                     //    //case "SQRT":
 
                     //    //    if (decimal.TryParse(tuple.Item2, out number))
@@ -330,21 +322,10 @@ namespace CalculatorCore
                     //Изначально вот этой строки: else return computeBinaryDictionary[MemoryState.Operation].Invoke(MemoryState.CurrentState, number);  --- не было
                     #endregion
                 }
-                else 
-                {
-                    //Сломана, если ввести цифру и нажать равно, то выскочит null ексепшн, по идее нет MemoryState.Operation, но его перехват через if ничего не дает
-                    return computeBinaryDictionary[MemoryState.Operation].Invoke(MemoryState.CurrentState, number);
-                }
-
+                else return constantDictionary[operation].Invoke();
             }
-            else return String.Empty;
-
-            
+            else return String.Empty;           
         }
-
-        //private static object Negate(decimal number) => -number;
-        private string Negate(decimal number) => (-number).ToString();
-
         #region Старое вычисление бинарных операций через знак "="
         //private object ComputeBinary(decimal number)
         //{
